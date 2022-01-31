9a372b241041ac8263499d31c20b590682f9977f
Mitchell Krawiec-Thayer <>
2022-01-31 11:26:27 -0700

    tweak: bump version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='isthmuslib',
-    version='0.0.49',
+    version='0.0.51',
     license='LGPL-3.0-only',
     description='Convenience utils for plotting, styling, and manipulating high-dimensional vectors',
     long_description='%s\n%s' % (
