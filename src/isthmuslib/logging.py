a1dd43695027e5654ff5ba990f314bf1984b7c5b
mitchellpkt <mitchellpkt+github@protonmail.com>
2022-03-16 11:17:37 -0600

    tweak: more efficient reshaping

--- src/isthmuslib/logging.py
+++ src/isthmuslib/logging.py
@@ -6,6 +6,8 @@ from .vectors import VectorSequence, VectorMultiset
 from .utils import divvy_workload, get_num_workers
 from pydantic import BaseModel
 from multiprocessing import Pool
+from time import perf_counter
+
 
 class LogIO(BaseModel):
     record_delimiter: str = '[@@@]'
@@ -122,7 +124,8 @@ def multi_chunk_processor_lambda(args: Tuple[List[str], str, str, str]) -> List[
 
 def auto_extract_from_text(input_string: str, return_type: str = 'dataframe', left_token: str = None,
                            key_value_delimiter: str = None, right_token: str = None, basis_col_name: str = None,
-                           record_delimiter: str = None, parallelize: Union[bool, int] = False,
+                           record_delimiter: str = None, parallelize_read: Union[bool, int] = False,
+                           parallelize_processing: Union[bool, int] = False,
                            disable_progress_bar: bool = None) -> Union[pd.DataFrame, VectorSequence, VectorMultiset]:
     """
     Extracts a data frame from a string
@@ -142,7 +145,8 @@ def auto_extract_from_text(input_string: str, return_type: str = 'dataframe', le
     :param right_token: right side of a record
     :param basis_col_name: optional, specify a basis column name for a VectorSequence (otherwise ignored)
     :param return_type: what format to return the data in
-    :param parallelize: whether to parallelize calculations. Can be an integer (# workers) or bool True / False
+    :param parallelize_read: whether to parallelize reading. Can be an integer (# workers) or bool True / False
+    :param parallelize_processing: whether to parallelize flattening. Can be an integer (# workers) or bool True / False
     :param disable_progress_bar: pass anything True to silence the progress bar
     :return: pandas.DataFrame or VectorMultiset or VectorSequence
     """
@@ -161,13 +165,12 @@ def auto_extract_from_text(input_string: str, return_type: str = 'dataframe', le
     if not record_delimiter:
         record_delimiter = LogIO().record_delimiter
 
-    df_output: pd.DataFrame = pd.DataFrame()
     record_chunks: List[str] = input_string.split(record_delimiter)[1:]
     chunk_buffers: List[Dict[str, Any]] = []
 
     # Process the chunks
-    num_workers: int = get_num_workers(parallelize_arg=parallelize)
-    if parallelize and (num_workers > 1):
+    num_workers: int = get_num_workers(parallelize_arg=parallelize_read)
+    if parallelize_read and (num_workers > 1):
         batches: List[List[Any]] = divvy_workload(num_workers=num_workers, tasks=record_chunks)
         i: List[Tuple[List[str], str, str, str]] = [(b, left_token, key_value_delimiter, right_token) for b in batches]
         with Pool(num_workers) as pool:
@@ -187,22 +190,48 @@ def auto_extract_from_text(input_string: str, return_type: str = 'dataframe', le
     # reshaped: Dict[str, List[Any]] = {key: [chunk.get(key, None) for chunk in chunk_buffers] for key in all_keys_flat}
     # df_output: pd.DataFrame = pd.DataFrame(reshaped)
 
-    for chunk_buffer in chunk_buffers:
-        if chunk_buffer:
-            df_output = pd.concat([df_output, pd.DataFrame(chunk_buffer, index=[-1])], ignore_index=True)
+    num_reshape_workers: int = get_num_workers(parallelize_arg=parallelize_processing)
+    print('   reshaping...')
+    tic: float = perf_counter()
+    if parallelize_processing and (num_reshape_workers > 1):
+        print('   ... in parallel')
+        batches: List[List[Any]] = divvy_workload(num_workers=num_reshape_workers, tasks=chunk_buffers)
+        with Pool(num_reshape_workers) as pool:
+            dataframes: List[pd.DataFrame] = pool.map(func=dicts_to_dataframe, iterable=batches)
+        df: pd.DataFrame = pd.concat(dataframes, ignore_index=True)
+    else:
+        print('   ... in serial')
+        df: pd.DataFrame = pd.DataFrame()
+        for chunk_buffer in chunk_buffers:
+            if chunk_buffer:
+                df: pd.DataFrame = pd.concat([df, pd.DataFrame(chunk_buffer, index=[-1])], ignore_index=True)
+    print(f'... Reshaping done in {perf_counter() - tic:.4f} seconds')
 
     if 'dataframe' in return_type.lower():
-        return df_output
+        return df
     elif 'multiset' in return_type.lower():
-        return VectorMultiset(data=df_output)
+        return VectorMultiset(data=df)
     elif 'sequence' in return_type.lower():
-        return VectorSequence(data=df_output, basis_col_name=basis_col_name)
+        return VectorSequence(data=df, basis_col_name=basis_col_name)
+
+
+def dicts_to_dataframe(dictionaries: List[Dict[str, Any]]) -> pd.DataFrame:
+    """
+    Helper function that converts a list of dictionaries into a dataframe (each dictionary = 1 row)
+
+    :param dictionaries: list of dictionaries, with one value per key per row
+    :return: dataframe representation
+    """
+    df: pd.DataFrame = pd.DataFrame()
+    for dictionary in [d for d in dictionaries if d]:
+        df = pd.concat([df, pd.DataFrame(dictionary, index=[-1])], ignore_index=True)
+    return df
 
 
 def auto_extract_from_file(file_path: Union[str, pathlib.Path], record_delimiter: str = None,
                            left_token: str = None, key_value_delimiter: str = None, right_token: str = None,
                            basis_col_name: str = None, disable_progress_bar: bool = None,
-                           parallelize: Union[bool, int] = False,
+                           parallelize_read: Union[bool, int] = False, parallelize_processing: Union[bool, int] = False,
                            return_type: str = 'dataframe') -> Union[pd.DataFrame, VectorSequence, VectorMultiset]:
     """
     Extracts a data frame from a file
@@ -223,6 +252,8 @@ def auto_extract_from_file(file_path: Union[str, pathlib.Path], record_delimiter
     :param basis_col_name: optional, specify a basis column name for a VectorSequence (otherwise ignored)
     :param return_type: what format to return the data in
     :param disable_progress_bar: pass anything True to silence the progress bar
+    :param parallelize_read: whether to parallelize reading. Can be an integer (# workers) or bool True / False
+    :param parallelize_processing: whether to parallelize flattening. Can be an integer (# workers) or bool True / False
     :return: pandas.DataFrame or VectorMultiset or VectorSequence
     """
 
@@ -245,7 +276,8 @@ def auto_extract_from_file(file_path: Union[str, pathlib.Path], record_delimiter
         input_string: str = f.read()
 
     return auto_extract_from_text(input_string=input_string, return_type=return_type,
-                                  record_delimiter=record_delimiter, left_token=left_token, parallelize=parallelize,
+                                  record_delimiter=record_delimiter, left_token=left_token,
+                                  parallelize_read=parallelize_read, parallelize_processing=parallelize_processing,
                                   key_value_delimiter=key_value_delimiter, right_token=right_token,
                                   basis_col_name=basis_col_name, disable_progress_bar=disable_progress_bar)
 
